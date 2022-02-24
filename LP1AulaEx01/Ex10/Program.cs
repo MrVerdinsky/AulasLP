d594f935f8df69dc9d62ab91e63304c14e78dae4
mrverdinsky <goncalo.vila.verde@hotmail.com>
2022-02-24 13:37:46 +0000

    First Commit

---
+++ LP1AulaEx01/Ex10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool x = true;
+            Console.WriteLine("Write 'exit' to close program");
+            Console.WriteLine("");
+            while (x)
+            {
+                
+                Console.Write("Please input something: ");
+                string input = Console.ReadLine();
+                Console.WriteLine("");
+                if (input == "exit")
+                {
+                    x = false;
+                    Console.WriteLine("Bye bye!");
+                }
+                else
+                {
+                    Console.WriteLine($"You wrote this: {input}");
+                    Console.WriteLine("");
+                } 
+            }
+        }
+
+    }
+}
