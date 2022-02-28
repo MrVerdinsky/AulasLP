46abd078c5395038888502f521cbe69dd4e57be0
MrVerdinsky <goncalo.vila.verde@hotmail.com>
2022-02-28 17:53:23 +0000

    Add Ex15

---
+++ LP1AulaEx01/Ex15/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ex15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You used this arguments:");
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine($"Argument {i+1}: {args[i]}");
+            }
+            
+        }
+    }
+}
