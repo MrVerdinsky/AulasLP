2b4e10dfe93a5cc1b9286b068a84a9889fd4e174
MrVerdinsky <goncalo.vila.verde@hotmail.com>
2022-02-28 15:39:34 +0000

    Add Ex11

---
+++ LP1AulaEx01/Ex11/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Ex11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variable used in the input cycle
+            bool x = true;
+            
+
+            Console.WriteLine("Write 'exit' to close program");
+            Console.WriteLine("");
+
+            //Cycle used to keep asking the user while "exit" is not written
+            while (x)
+            {
+                string reversedString = "";
+                Console.Write("Please input something: ");
+                string input = Console.ReadLine();
+
+                //Cycle used to reverse input by user
+                for (int i = input.Length - 1 ; i >= 0; i--)
+                {
+                    reversedString += input[i]; 
+                }
+
+                Console.WriteLine("");
+
+                //Checks if the user wrote "exit" as input
+                if (input == "exit")
+                {
+                    x = false;
+                    Console.WriteLine("Bye bye!");
+                }
+                else
+                {
+                    Console.WriteLine($"You wrote this but its reversed: {reversedString}");
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
