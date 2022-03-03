fb8de714271e9f8dbf4f5ae518e84924ff53172e
Gonçalo VilaVerde <a21901395@alunos.ulht.pt>
2022-03-03 11:43:36 +0100

    Add ValoresEspeciais - All types Min/Max Values

---
+++ Lp1Aula02/ValoresEspeciais/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //All type Min Values
+            byte a = byte.MinValue;
+            short b = short.MinValue;
+            int c = int.MinValue;
+            long d = long.MinValue;
+            double e = double.MinValue;
+            float f = float.MinValue;
+            char g = char.MinValue;
+
+            //All type Max Values
+            byte a2 = byte.MaxValue;
+            short b2 = short.MaxValue;
+            int c2 = int.MaxValue;
+            long d2 = long.MaxValue;
+            double e2 = double.MaxValue;
+            float f2 = float.MaxValue;
+            char g2 = char.MaxValue;
+
+            Console.WriteLine("Min Values of all Types:");
+            Console.WriteLine($"    Byte Min Value is: {a}");
+            Console.WriteLine($"    Short Min Value is: {b}");
+            Console.WriteLine($"    Int Min Value is: {c}");
+            Console.WriteLine($"    Long Min Value is: {d}");
+            Console.WriteLine($"    Double Min Value is: {e}");
+            Console.WriteLine($"    Float Min Value is: {f}");
+            Console.WriteLine($"    Char Min Value is: {g}");
+            Console.WriteLine("");
+            Console.WriteLine("Max Values of all Types:");
+            Console.WriteLine($"    Byte Max Value is: {a2}");
+            Console.WriteLine($"    Short Max Value is: {b2}");
+            Console.WriteLine($"    Int Max Value is: {c2}");
+            Console.WriteLine($"    Long Max Value is: {d2}");
+            Console.WriteLine($"    Double Max Value is: {e2}");
+            Console.WriteLine($"    Float Max Value is: {f2}");
+            Console.WriteLine($"    Char Max Value is: {g2}");
+        }
+    }
+}
