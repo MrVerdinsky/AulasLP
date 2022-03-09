99c46f3508fa31e07d903f32a790e222857eb4f7
MrVerdinsky <goncalo.vila.verde@hotmail.com>
2022-03-09 19:51:15 +0000

    Add PercorrerString Ex

---
+++ LP1Aula03/PercorreString/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace PercorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask user for string
+            //Ask user a character
+            //Print string without character chosen
+           
+            while (true)
+            {
+                bool valueString = false;
+                bool valueChar = false;
+                string inputString = " ";
+                string newString = " ";
+                char validChar = ' ';
+
+                while (!valueString)
+                {
+                    Console.Write("Insert String: ");
+                    inputString = Console.ReadLine();
+                    if(inputString != " ") valueString = true;
+                    if(inputString == "exit") System.Environment.Exit(0);
+                }
+
+                while (!valueChar)
+                {
+                    string inputChar;
+                    Console.Write("Insert Char to Eliminate: ");
+                    if (!char.TryParse(inputChar = Console.ReadLine(), out validChar))
+                    {
+                        Console.WriteLine("Invalid Value");
+                    }
+                    else valueChar = true; 
+                }
+
+                for (int i = 0; i < inputString.Length; i++)
+                {
+                    
+                    if (inputString[i] == validChar) continue;
+
+                    else newString += inputString[i];
+
+
+                }
+                Console.WriteLine("");
+                Console.WriteLine($"Input String: {inputString}");
+                Console.WriteLine($"Input Char: {validChar}");
+                Console.WriteLine($"New String with Char Removed: {newString}");
+                Console.WriteLine("");
+            }
+
+        }
+    }
+}
