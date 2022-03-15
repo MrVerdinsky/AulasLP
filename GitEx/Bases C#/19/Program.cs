c57ddec869d908a1fd3e04d457f043e979fde751
MrVerdinsky <goncalo.vila.verde@hotmail.com>
2022-03-15 16:23:45 +0000

    Add Ex19

---
+++ GitEx/Bases C#/19/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ex19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x = 0f;
+            float y = 0f;
+            string print = "";
+            Console.WriteLine("Input 1st Number:");
+            x = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input 2nd Number:");
+            y = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"HighScore: {x}");
+            Console.WriteLine($"Score: {y}");
+
+           print = x < y ? "New Highscore" : "Keep Trying!";
+           
+           Console.WriteLine(print);
+        }
+    }
+}
---
+++ GitEx/Bases C#/Ex18/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ex18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            int charCounter = 0;
+            Console.WriteLine("Input String:");
+            input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                charCounter++;
+            }
+
+            if (charCounter > 20)
+            {
+                Console.WriteLine("Lol Too long didnt read!");
+            }
+            else if (charCounter > 5 && charCounter < 20)
+            {
+                Console.WriteLine("Ok, if you say so");
+            }
+            else
+            {
+                Console.WriteLine("Thats not very meanigful, is it?");
+            }
+        }
+    }
+}
