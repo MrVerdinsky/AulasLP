c2b88dd1fed96c13fd607d47be7c8de03d3b933d
MrVerdinsky <goncalo.vila.verde@hotmail.com>
2022-03-15 15:59:41 +0000

    Add ex17

---
+++ GitEx/Bases C#/Ex17/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            Console.WriteLine("Input a number:");
+            x = int.Parse(Console.ReadLine());
+
+            if (x % 3 == 0 && x % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz!");
+            }
+            else if (x % 5 == 0) Console.WriteLine("Buzz!");
+            else if (x % 3 == 0) Console.WriteLine("Fizz!");
+            else Console.WriteLine($"{x}!");
+        }
+    }
+}
